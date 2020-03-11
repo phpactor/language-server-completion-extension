d6eb3f8deb5d245a22195048324327090cba3dad
Przepompownia <przepompownia@users.noreply.github.com>
2020-03-11 20:20:59 +0100

    Add tests

---
+++ tests/Unit/Util/SuggestionLabelFormatterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Phpactor\Extension\LanguageServerCompletion\Tests\Unit\Util;
+
+use Phpactor\Completion\Core\Suggestion;
+use Phpactor\Extension\LanguageServerCompletion\Util\SuggestionLabelFormatter;
+use PHPUnit\Framework\TestCase;
+
+class SuggestionLabelFormatterTest extends TestCase
+{
+    /**
+     * @var SuggestionLabelFormatter
+     */
+    private $formatter;
+
+    protected function setUp(): void
+    {
+        $this->formatter = new SuggestionLabelFormatter();
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testFormat(string $type, string $label, string $expected)
+    {
+        $suggestion = new Suggestion('bar', $type, null, null, $label);
+
+        $this->assertSame($expected, $this->formatter->format($suggestion));
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            [Suggestion::TYPE_VARIABLE, '$foo', 'foo'],
+            [Suggestion::TYPE_FUNCTION, 'foo', 'foo('],
+            [Suggestion::TYPE_FIELD, 'foo', 'foo'],
+        ];
+    }
+}
